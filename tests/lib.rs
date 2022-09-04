2a562f1d34a54c5852ae9e70a5ec9d2d206d5755
Andreas Razmyslov <andreas.razmyslov@gmail.com>
2022-09-04 16:04:59 +0200

    Upgrade to 0.5.0

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,32 +1,37 @@
 use radix_engine::ledger::*;
-use radix_engine::transaction::*;
+use radix_engine::model::extract_package;
+use scrypto::core::NetworkDefinition;
 use scrypto::prelude::*;
+use scrypto_unit::*;
+use transaction::builder::ManifestBuilder;
 
 #[test]
 fn test_hello() {
-    // Set up environment.
-    let mut ledger = InMemorySubstateStore::with_bootstrap();
-    let mut executor = TransactionExecutor::new(&mut ledger, false);
-    let (pk, sk, account) = executor.new_account();
-    let package = executor.publish_package(compile_package!()).unwrap();
+    // Setup the environment
+    let mut store = TypedInMemorySubstateStore::with_bootstrap();
+    let mut test_runner = TestRunner::new(true, &mut store);
+
+    // Create an account
+    let (public_key, _private_key, account_component) = test_runner.new_account();
+
+    // Publish package
+    let package_address = test_runner.publish_package(extract_package(compile_package!()).unwrap());
 
     // Test the `instantiate_hello` function.
-    let transaction1 = TransactionBuilder::new()
-        .call_function(package, "Hello", "instantiate_hello", args![])
-        .build(executor.get_nonce([pk]))
-        .sign([&sk]);
-    let receipt1 = executor.validate_and_execute(&transaction1).unwrap();
-    println!("{:?}\n", receipt1);
-    assert!(receipt1.result.is_ok());
+    let manifest = ManifestBuilder::new(&NetworkDefinition::local_simulator())
+        .call_function(package_address, "Hello", "instantiate_hello", args!())
+        .build();
+    let receipt = test_runner.execute_manifest_ignoring_fee(manifest, vec![public_key]);
+    println!("{:?}\n", receipt);
+    receipt.expect_commit_success();
+    let component = receipt.expect_commit().entity_changes.new_component_addresses[0];
 
     // Test the `free_token` method.
-    let component = receipt1.new_component_addresses[0];
-    let transaction2 = TransactionBuilder::new()
-        .call_method(component, "free_token", args![])
-        .call_method_with_all_resources(account, "deposit_batch")
-        .build(executor.get_nonce([pk]))
-        .sign([&sk]);
-    let receipt2 = executor.validate_and_execute(&transaction2).unwrap();
-    println!("{:?}\n", receipt2);
-    assert!(receipt2.result.is_ok());
+    let manifest = ManifestBuilder::new(&NetworkDefinition::local_simulator())
+        .call_method(component, "free_token", args!())
+        .call_method_with_all_resources(account_component, "deposit_batch")
+        .build();
+    let receipt = test_runner.execute_manifest_ignoring_fee(manifest, vec![public_key]);
+    println!("{:?}\n", receipt);
+    receipt.expect_commit_success();
 }
